5642c32f3c3ad0e6db6ac3616e6812b90454a9eb
nathaniel733 <159222079+nathaniel733@users.noreply.github.com>
2026-09-17 15:36:06 +0530

    Update client ID and secret in sts.js
    Replaced process.env variables with hardcoded client ID and secret.

--- api/users/sts.js
+++ api/users/sts.js
@@ -39,8 +39,8 @@ export default function handler(req, res) {
       });
     }
 
-    const clientId = process.env.CLIENT_ID;
-    const clientSecret = process.env.CLIENT_SECRET;
+    const clientId = "cs-8382e2d8-23af-560c-b576-08cc20c52cb8";
+    const clientSecret = "LonqHE7zddsy01RkrCN0+o+bNqVQ51Vh2FdcJrRrlQU=";
 
     if (!clientId || !clientSecret) {
       console.error("CLIENT_ID or CLIENT_SECRET is missing.");
